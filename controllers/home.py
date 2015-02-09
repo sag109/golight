d9f674ab6997f336fd4ec0cbacfaa526fcdd83b4
Peter Mash <PeterMash@email.com>
2015-02-09 06:41:35 -0500

    oops. trying with users

--- controllers/home.py
+++ controllers/home.py
@@ -2,7 +2,13 @@ import webapp2
 import os
 
 from google.appengine.ext.webapp import template
+from google.appengine.api import users
 
 class RenderHome(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write("templates/index.html")
+        user = users.get_current_user()
+        if user:
+            self.response.out.write('logged in!')
+        else:
+            self.response.out.write('not logged in')
+
