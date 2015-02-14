f5a34c38ed2ebbfd5396f0e8bea5978bce5ca5e8
nap54 <nicholas.a.peluso@gmail.com>
2015-02-14 15:54:26 -0500

    Added profile.py post, edited profile.py. Edited profile template.

--- controllers/profile.py
+++ controllers/profile.py
@@ -12,7 +12,6 @@ class RenderProfile(webapp2.RequestHandler):
        # self.response.out.write(template.render("templates/test.html",{"controller": controller}))
        user= users.get_current_user()
        if user:
-            self.response.out.write('<html><body>')
             allUsers= user_info.all()
             userExists= False
             newUser = user_info()
@@ -29,13 +28,49 @@ class RenderProfile(webapp2.RequestHandler):
                 newUser.availability= "Success"
                 newUser.put()
 
+            template_params = {
+                "usernickname": newUser.name()
+                "status": newUser.availability
+            }
+            render_template(self, 'output.html', template_params)
+
+    def post(self):
+        # controller = "login.py"
+        # self.response.out.write(template.render("templates/test.html",{"controller": controller}))
+        user= users.get_current_user()
+        if user:
+            allUsers= user_info.all()
+            userExists= False
+            newUser = user_info()
+            for person in allUsers:
+                if person.email == user.email():
+                    userExists= True
+                    newUser= person
+            if not userExists:
+                self.response.out.write('making a new user')
+                newUser.name= user.nickname()
+                newUser.email= user.email()
+                newUser.friendList.append(user.email())
+                newUser.friendList.append("dontbelonely@pitt.edu")
+                newUser.availability= "Success"
+                newUser.put()
+
+            input= self.request.get("status");
+            if(input == "update")
+                if(newUser.availability() == "Success")
+                    newUser.availability = "Danger"
+                else if(newUser.availability() == "Danger")
+                    newUser.availability = "Warning"
+                else if(newUser.availability() == "Warning")
+                    newUser.availability = "Success"
+                else
+                    newUser.availability = "Default"
+                newUser.put()
+                
+            template_params = {
+                "usernickname": newUser.nickname()
+                "status": newUser.availability
+            }
+            render_template(self, 'output.html', template_params)
             
-            self.response.out.write('Hello, ' + user.nickname() + '!<br><br>')
-            self.response.out.write(newUser.email+'<br>')
-
-            for pal in newUser.friendList:
-                self.response.out.write(pal+'<br>')
-            self.response.out.write('</body></html>')
-
-
 
