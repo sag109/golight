8d0412d7acc0656974c6bf8e7fe894c5be6e5054
Shelley Goldberg <sag109@pitt.edu>
2015-02-14 12:57:50 -0500

    removed main

--- controllers/main.py
+++
@@ -1,9 +0,0 @@
-import webapp2
-import os
-
-from google.appengine.ext.webapp import template
-
-class RenderMain(webapp2.RequestHandler):
-    def get(self):
-        controller = "main.py"
-        self.response.out.write(template.render("templates/test.html",{"controller": controller}))
