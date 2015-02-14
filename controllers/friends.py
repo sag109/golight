4b80297f7b498ba627be9396464485c0cdb324cd
Shelley Goldberg <sag109@pitt.edu>
2015-02-14 14:16:18 -0500

    update friends.py to match template

--- controllers/friends.py
+++ controllers/friends.py
@@ -2,8 +2,37 @@ import webapp2
 import os
 
 from google.appengine.ext.webapp import template
+from google.appengine.api import users
+from google.appengine.ext import db
+from Account import user_info
 
 class RenderFriends(webapp2.RequestHandler):
     def get(self):
-        controller = "friends.py"
-        self.response.out.write(template.render("templates/test.html",{"controller": controller}))
+        #parameter is friends
+        user= users.get_current_user()
+        if user:
+            userExists= False
+            newUser = user_info()
+            allUsers= user_info.all()
+            for person in allUsers:
+                if person.email == user.email():
+                    userExists= True
+                    newUser= person
+            if not userExists:
+                #self.response.out.write('making a new user')
+                newUser.name= user.nickname()
+                newUser.email= user.email()
+                newUser.friendList.append(user.email())#they are their own friend
+                newUser.friendList.append("dontbelonely@pitt.edu")
+                newUser.availability= "Success"
+                newUser.put()
+            friends=[]
+            for friend in newUser.friendList:
+                for person in allUsers:
+                    if person.email == friend:
+                        #emails match
+                        friends.append(person)
+            #for pal in friends:
+             #   self.response.out.write(pal.name+'<br>')
+            self.response.out.write(template.render("templates/friends.html",{"friends": friends}))
+
---
+++ controllers/profile.py
@@ -0,0 +1,41 @@
+import webapp2
+import os
+
+from Account import user_info
+from google.appengine.ext.webapp import template
+from google.appengine.ext import db
+from google.appengine.api import users
+
+class RenderProfile(webapp2.RequestHandler):
+    def get(self):
+       # controller = "login.py"
+       # self.response.out.write(template.render("templates/test.html",{"controller": controller}))
+       user= users.get_current_user()
+       if user:
+            self.response.out.write('<html><body>')
+            allUsers= user_info.all()
+            userExists= False
+            newUser = user_info()
+            for person in allUsers:
+                if person.email == user.email():
+                    userExists= True
+                    newUser= person
+            if not userExists:
+                self.response.out.write('making a new user')
+                newUser.name= user.nickname()
+                newUser.email= user.email()
+                newUser.friendList.append(user.email())
+                newUser.friendList.append("dontbelonely@pitt.edu")
+                newUser.availability= "Success"
+                newUser.put()
+
+            
+            self.response.out.write('Hello, ' + user.nickname() + '!<br><br>')
+            self.response.out.write(newUser.email+'<br>')
+
+            for pal in newUser.friendList:
+                self.response.out.write(pal+'<br>')
+            self.response.out.write('</body></html>')
+
+
+
